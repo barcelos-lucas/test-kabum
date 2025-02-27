ee534d17640d5e78036f744625ce6b3cb337a603
Lucas Barcelos <lucavm87@gmail.com>
2025-02-27 15:22:14 -0300

    chore: checkpoint de teste no ambiente de produção 2

--- public/index.php
+++ public/index.php
@@ -1,11 +1,2 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-// require_once __DIR__ . '/../app/config/database.php';
-
-// header("Location: formsLogin.html");
-// exit;
-
-echo "Testando!";
+ echo "teste railway!";
