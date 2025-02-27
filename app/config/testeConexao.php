4c98796580ffd137581fabc904bd9e74bcca7ae6
Lucas Barcelos <lucavm87@gmail.com>
2025-02-27 15:10:04 -0300

    fix: update na rota de conexão

--- app/config/testeConexao.php
+++ app/config/testeConexao.php
@@ -1,14 +1,10 @@
 <?php
-// Incluir o arquivo de configuração do banco de dados
-require_once 'database.php'; // Ambos estão na mesma pasta, então basta usar o nome do arquivo diretamente
+require_once 'database.php'; 
 
-// Criar uma instância da classe Database
 $database = new Database();
 
-// Tentar conectar ao banco de dados
 $conn = $database->conectarBanco();
 
-// Verificar se a conexão foi bem-sucedida
 if ($conn) {
     echo "Conexão com o banco de dados bem-sucedida!";
 } else {
