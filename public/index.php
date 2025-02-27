53e25a6c332aa4f2e1c8feed05099c86f87c21b2
Lucas Barcelos <lucavm87@gmail.com>
2025-02-27 15:09:31 -0300

    fix: corrige o index para rota principal

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . '/app/config/database.php';
+require_once __DIR__ . '/../app/config/database.php';
 
-header("Location: ../app/config/formsLogin.html");
+header("Location: formsLogin.html");
 exit;
