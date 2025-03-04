47b7f374fa4f2e0f37be1fe7565ecedfe2f7cef0
Lucas Barcelos <lucavm87@gmail.com>
2025-03-03 23:15:10 -0300

    fix: finaliza a duplicidade de conexão do banco de dados

--- app/config/database.php
+++ app/config/database.php
@@ -23,17 +23,5 @@ class Database {
         $this->password = $_ENV['DB_PASS'];
         $this->port = $_ENV['DB_PORT'];
     }
-
-    function conectarBanco() {
-        try {
-            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";port=" . $this->port;
-            $this->conn = new PDO($dsn, $this->username, $this->password, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]);
-            return $this->conn;
-        } catch (PDOException $e) {
-            die("Erro na conexão: " . $e->getMessage());
-        }
-    }
+   
 }
