0642a8d992f537f385eb2099fbdbf9f746fcfb13
Lucas Barcelos <lucavm87@gmail.com>
2025-03-05 21:45:57 -0300

    deploy: rollback na config do bd

--- app/config/database.php
+++ app/config/database.php
@@ -10,49 +10,33 @@ $dotenv = Dotenv::createImmutable(__DIR__ . '/../../', '.env', true);
 $dotenv->load();
 
 
- 
-$DB_HOST=$_ENV["DB_HOST"];
-$DB_USER=$_ENV["DB_USER"];
-$DB_PASS=$_ENV["DB_PASS"];
-$DB_NAME=$_ENV["DB_NAME"];
-$DB_PORT=$_ENV["DB_PORT"];
 
+class Database {
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $port;
+    public $conn;
 
-$db=mysqli_connect("$DB_HOST","$DB_USER","$DB_PASS","$DB_NAME","$DB_PORT");
+    function __construct() {
+        $this->host = $_ENV['DB_HOST'];
+        $this->db_name = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+        $this->port = $_ENV['DB_PORT'];
+    }
 
-
-if (mysqli_connect_errno()) {
-  die("Erro na conexão: " . mysqli_connect_error());
+    function conectarBanco() {
+        try {
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";port=" . $this->port;
+            $this->conn = new PDO($dsn, $this->username, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+            return $this->conn;
+        } catch (PDOException $e) {
+            die("Erro na conexão: " . $e->getMessage());
+        }
+    }
 }
-
-
-
-// class Database {
-//     private $host;
-//     private $db_name;
-//     private $username;
-//     private $password;
-//     private $port;
-//     public $conn;
-
-//     function __construct() {
-//         $this->host = $_ENV['DB_HOST'];
-//         $this->db_name = $_ENV['DB_NAME'];
-//         $this->username = $_ENV['DB_USER'];
-//         $this->password = $_ENV['DB_PASS'];
-//         $this->port = $_ENV['DB_PORT'];
-//     }
-
-//     function conectarBanco() {
-//         try {
-//             $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";port=" . $this->port;
-//             $this->conn = new PDO($dsn, $this->username, $this->password, [
-//                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,  
-//                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-//             ]);
-//             return $this->conn;
-//         } catch (PDOException $e) {
-//             die("Erro na conexão: " . $e->getMessage());
-//         }
-//     }
-// }
